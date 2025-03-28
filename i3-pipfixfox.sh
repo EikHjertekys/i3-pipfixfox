89979884197507deada300394a2a0a8fa725135f
Eik Hjertekys <eikhjertekys@protonmail.com>
2025-03-28 15:05:35 +0100

    Update and rename i3-piptile.sh to i3-pipfixfox.sh

--- i3-pipfixfox.sh
+++ i3-pipfixfox.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# | https://github.com/EikHjertekys/i3-piptile
+# | https://github.com/EikHjertekys/i3-pipfixfox
 # | 28 / 03 / 25
 
 # Release i3 keybind
@@ -12,7 +12,7 @@ current_layout=$(setxkbmap -query | sed -n 's/layout:\s*\(.*\)/\1/p')
 # Switch to US layout
 setxkbmap -layout us -option ''
 
-# Trigger PIP shortcut (Ctrl+Shift+])
+# Trigger PiP shortcut ( Ctrl+Shift+] )
 xdotool key Ctrl+Shift+bracketright
 
 # Take a nap
@@ -21,5 +21,5 @@ sleep 0.2
 # Switch back to original layout
 setxkbmap -layout $current_layout -option ''
 
-# i3 : Tile PIP
+# i3 : Tile PiP
 xdotool key super+Shift+space
